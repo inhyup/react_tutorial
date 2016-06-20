6abaaa27d7134955e1d88b9a88ee3c2672173e94
Inhyup Kim <hyup3569@gmail.com>
2016-06-20 12:59:26 -0600

    create some components

--- public/scripts/example.jsx
+++ public/scripts/example.jsx
@@ -1,18 +1,66 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React from 'react'
+import {render} from 'react-dom'
 
 var CommentBox = React.createClass({
 
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world!!
+        <h1>Comments</h1>
+        <CommentList data={this.props.data} />
+        <CommentForm />
       </div>
-    );
+    )
   }
-});
+})
 
-ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('content')
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      )
+    })
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    )
+  }
+})
+
+var CommentForm = React.createClass({
+  render: function() {
+    return(
+      <div className="commentForm">
+        CommentForm
+      </div>
+    )
+  }
+})
+
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className = "comment">
+        <h2 className = "commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    )
+  }
+})
+
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+render(
+  <CommentBox data={data}/>,
+  document.getElementById('container')
 );
